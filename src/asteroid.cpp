4d57c151ed68fdf7fbdc7ac5aad2fb4862c1189e
Tóth Nikolett <nikolett.toth1204@gmail.com>
2019-03-20 15:07:42 +0100

    Asteroid class: shape implemented

--- src/asteroid.cpp
+++ src/asteroid.cpp
@@ -17,28 +17,28 @@ void Asteroid::paint(QPainter *painter, const QStyleOptionGraphicsItem *item, QW
 
     if (this->life != 0) {
         if (this->life == 3) {
-            painter->drawRect(this->getX(), this->getY(), 2, 2);
+            painter->drawRect(this->m_x, this->m_y, 2, 2);
         } else if (this->life == 2) {
             if (this->type != 0) {
-                painter->drawRect(this->getX(), this->getY(), 1, 1);
+                painter->drawRect(this->m_x, this->m_y, 1, 1);
             }
             if (this->type != 1) {
-                painter->drawRect(this->getX()+1, this->getY(), 1, 1);
+                painter->drawRect(this->m_x+1, this->m_y, 1, 1);
             }
             if (this->type != 2) {
-                painter->drawRect(this->getX()+1, this->getY()+1, 1, 1);
+                painter->drawRect(this->m_x+1, this->m_y+1, 1, 1);
             }
             if (this->type != 3) {
-                painter->drawRect(this->getX(), this->getY()+1, 1, 1);
+                painter->drawRect(this->m_x, this->m_y+1, 1, 1);
             }
         } else if (this->life == 1) {
             if (this->type != 0 || this->type != 2) {
-                painter->drawRect(this->getX(), this->getY(), 1, 1);
-                painter->drawRect(this->getX()+1, this->getY()+1, 1, 1);
+                painter->drawRect(this->m_x, this->m_y, 1, 1);
+                painter->drawRect(this->m_x+1, this->m_y+1, 1, 1);
             }
             if (this->type != 1 || this->type != 3) {
-                painter->drawRect(this->getX()+1, this->getY(), 1, 1);
-                painter->drawRect(this->getX(), this->getY()+1, 1, 1);
+                painter->drawRect(this->m_x+1, this->m_y, 1, 1);
+                painter->drawRect(this->m_x, this->m_y+1, 1, 1);
             }
         }
     }
@@ -49,6 +49,36 @@ QRectF Asteroid::boundingRect() const {
 }
 
 QPainterPath Asteroid::shape() const {
+    QPainterPath path;
+    if (this->life != 0) {
+        if (this->life == 3) {
+            path.addRect(this->m_x, this->m_y, 2, 2);
 
+        } else if (this->life == 2) {
+            if (this->type != 0) {
+                path.addRect(this->m_x, this->m_y, 1, 1);
+            }
+            if (this->type != 1) {
+                path.addRect(this->m_x+1, this->m_y, 1, 1);
+            }
+            if (this->type != 2) {
+                path.addRect(this->m_x+1, this->m_y+1, 1, 1);
+            }
+            if (this->type != 3) {
+                path.addRect(this->m_x, this->m_y+1, 1, 1);
+            }
+        } else if (this->life == 1) {
+            if (this->type != 0 || this->type != 2) {
+                path.addRect(this->m_x, this->m_y, 1, 1);
+                path.addRect(this->m_x+1, this->m_y+1, 1, 1);
+            }
+            if (this->type != 1 || this->type != 3) {
+                path.addRect(this->m_x+1, this->m_y, 1, 1);
+                path.addRect(this->m_x, this->m_y+1, 1, 1);
+            }
+        }
+    }
+
+    return path;
 }
 

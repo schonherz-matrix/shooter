ca717b7bbd9ef33ab9c67621713b46f254d843bf
Adrián Robotka <robotka.adrian@gmail.com>
2019-03-20 18:04:15 +0100

    select gamepad

--- src/matrixscene.cpp
+++ src/matrixscene.cpp
@@ -1,5 +1,6 @@
 #include "matrixscene.h"
 #include "bar.h"
+#include <QtGamepad/qgamepad.h>
 
 MatrixScene::MatrixScene(QObject *parent)
     : QGraphicsScene(parent),
@@ -9,45 +10,55 @@ MatrixScene::MatrixScene(QObject *parent)
       player2HPBar(Qt::green, 1),
       player1PWBar(Qt::yellow, 0),
       player2PWBar(Qt::yellow, 0) {
-  // set BG
-  setBackgroundBrush(Qt::black);
+    // set BG
+    setBackgroundBrush(Qt::black);
 
-  // setSize
-  setSceneRect(0, 0, 32, 26);
+    // setSize
+    setSceneRect(0, 0, 32, 26);
 
-  // init Frame
-  out.pixels = Array2D<Color>(32, 26);
-  frame.fill(Qt::black);
-  for (int x = 0; x < out.pixels.getWidth(); x++) {
-    for (int y = 0; y < out.pixels.getHeight(); y++) {
-      out.pixels(x, y) = Color(0, 0, 0);
+    // init Frame
+    out.pixels = Array2D<Color>(32, 26);
+    frame.fill(Qt::black);
+    for (int x = 0; x < out.pixels.getWidth(); x++) {
+        for (int y = 0; y < out.pixels.getHeight(); y++) {
+          out.pixels(x, y) = Color(0, 0, 0);
+        }
     }
-  }
-  transmitter.sendFrame(out);
+    transmitter.sendFrame(out);
 
-  connect(this, &QGraphicsScene::changed, this, &MatrixScene::updateFrame);
+    connect(this, &QGraphicsScene::changed, this, &MatrixScene::updateFrame);
 
-  // init Timer
-  connect(&timer, SIGNAL(timeout()), this, SLOT(advance()));
-  timer.start(1000 / 33);
+    auto gamepads = QGamepadManager::instance()->connectedGamepads();
+    if (gamepads.isEmpty()) {
+        qDebug() << "Did not find any connected gamepads";
+        // TODO implement error handling
+        return;
+    }
 
-  // init Map
-  // set pos
-  player1HPBar.setPos(0, 0);
-  player2HPBar.setPos(31, 0);
-  player1PWBar.setPos(1, 0);
-  player2PWBar.setPos(30, 0);
+    auto upperPlayerGamePad = gamepads.first();
+    auto lowerPlayerGamePad = gamepads.last();
 
-  // add bars
-  addItem(&player1HPBar);
-  addItem(&player2HPBar);
-  addItem(&player1PWBar);
-  addItem(&player2PWBar);
-  upperPlayer = new Player(true, &player1HPBar);
-  lowerPlayer = new Player(false, &player2HPBar);
+    // init Timer
+    connect(&timer, SIGNAL(timeout()), this, SLOT(advance()));
+    timer.start(1000 / 33);
 
-  addItem(upperPlayer);
-  addItem(lowerPlayer);
+    // init Map
+    // set pos
+    player1HPBar.setPos(0, 0);
+    player2HPBar.setPos(31, 0);
+    player1PWBar.setPos(1, 0);
+    player2PWBar.setPos(30, 0);
+
+    // add bars
+    addItem(&player1HPBar);
+    addItem(&player2HPBar);
+    addItem(&player1PWBar);
+    addItem(&player2PWBar);
+    upperPlayer = new Player(true, &player1HPBar);
+    lowerPlayer = new Player(false, &player2HPBar);
+
+    addItem(upperPlayer);
+    addItem(lowerPlayer);
 }
 
 void MatrixScene::updateFrame() {

b169c1f3552fbdb3be77e9c649a1baf911a2e24b
Zsombor Bodnár <zsombor.bodnar@kszk.bme.hu>
2019-03-20 16:34:16 +0100

    Update bar, scale graphicsView

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,10 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "bar.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow), scene(this) {
   ui->setupUi(this);
   ui->graphicsView->setScene(&scene);
+  ui->graphicsView->scale(10, 10);
+  auto bar = new Bar(Qt::red, 1);
+  bar->setPos(0, 0);
+  scene.addItem(bar);
 }
 
 MainWindow::~MainWindow() { delete ui; }

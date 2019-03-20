21bad18e9ef9b090ef53c73c41c29ba9c962ec36
Tóth Nikolett <nikolett.toth1204@gmail.com>
2019-03-20 15:02:56 +0100

    Asteroid class: boundingRect and paint functions implemented

--- src/asteroid.cpp
+++ src/asteroid.cpp
@@ -1,6 +1,54 @@
 #include "asteroid.h"
 
-Asteroid::Asteroid()
-{
+Asteroid::Asteroid() {
+    this->type = QRandomGenerator::global()->bounded(0, 3);
+    this->color = Qt::darkGray;
+    this->life = Asteroid::MAXLIFE;
+}
+
+void Asteroid::paint(QPainter *painter, const QStyleOptionGraphicsItem *item, QWidget *widget) {
+    Q_UNUSED(item);
+    Q_UNUSED(widget);
+
+    painter->setPen(Qt::NoPen);
+
+    QBrush brush(this->color);
+    painter->setBrush(brush);
+
+    if (this->life != 0) {
+        if (this->life == 3) {
+            painter->drawRect(this->getX(), this->getY(), 2, 2);
+        } else if (this->life == 2) {
+            if (this->type != 0) {
+                painter->drawRect(this->getX(), this->getY(), 1, 1);
+            }
+            if (this->type != 1) {
+                painter->drawRect(this->getX()+1, this->getY(), 1, 1);
+            }
+            if (this->type != 2) {
+                painter->drawRect(this->getX()+1, this->getY()+1, 1, 1);
+            }
+            if (this->type != 3) {
+                painter->drawRect(this->getX(), this->getY()+1, 1, 1);
+            }
+        } else if (this->life == 1) {
+            if (this->type != 0 || this->type != 2) {
+                painter->drawRect(this->getX(), this->getY(), 1, 1);
+                painter->drawRect(this->getX()+1, this->getY()+1, 1, 1);
+            }
+            if (this->type != 1 || this->type != 3) {
+                painter->drawRect(this->getX()+1, this->getY(), 1, 1);
+                painter->drawRect(this->getX(), this->getY()+1, 1, 1);
+            }
+        }
+    }
+}
+
+QRectF Asteroid::boundingRect() const {
+    return QRectF(this->m_x, this->m_y, 2, 2);
+}
+
+QPainterPath Asteroid::shape() const {
 
 }
+
--- src/asteroid.h
+++ src/asteroid.h
@@ -1,11 +1,46 @@
 #ifndef ASTEROID_H
 #define ASTEROID_H
 
+#include <QGraphicsItem>
+#include <QPainter>
+#include <QRandomGenerator>
 
 class Asteroid : public QGraphicsItem
 {
+    Q_OBJECT
+    int m_x, m_y;
+    int type;
+
+    Q_PROPERTY(int x MEMBER m_x READ getX WRITE setX)
+    Q_PROPERTY(int y MEMBER m_y READ getY WRITE setY)
+
+    QColor color;
+
+    int life;
+
 public:
     Asteroid();
+
+    int getX() {
+        return this->m_x;
+    }
+
+    int getY() {
+        return this->m_y;
+    }
+
+    void setX(int x) {
+        m_x = x;
+    }
+
+    void setY(int y) {
+        m_y = y;
+    }
+
+    // QGraphicsItem interface
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *item, QWidget *widget) override;
+    QRectF boundingRect() const override;
+    QPainterPath shape() const override;
 };
 
 #endif // ASTEROID_H

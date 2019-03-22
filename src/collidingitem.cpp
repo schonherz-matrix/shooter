ead145d138b37e9d51a4b2138536c71191da2b18
Kiss Ádám <kiss.adam@simonyi.bme.hu>
2019-03-22 11:36:25 +0100

    Add comment in connection with delete oprator and QGraphicalItem

--- src/collidingitem.cpp
+++ src/collidingitem.cpp
@@ -35,4 +35,9 @@ void CollidingItem::remove(){
     //Story with simple delete____  deleting an object from the scene during the iteration of advance causes segfault.
     //TODO free memory somewhere else
     //This function is a placeholder for that function.
+
+    //https://forum.qt.io/topic/50300/solved-remove-an-item-from-the-child-items-list-of-qgraphicsitem/9
+    //https://github.com/qt/qtbase/blob/5.12/src/widgets/graphicsview/qgraphicsscene.cpp#L3302
+
+    //maybe an other 'advance' function is the soultion
 }

ee39deeff2679420deab5f61eeabebede0e12002
Kiss Adam <kiss.adam@simonyi.bme.hu>
2019-04-23 22:29:31 +0200

    Config: tune values

--- src/config.cpp
+++ src/config.cpp
@@ -6,8 +6,8 @@ using namespace std::literals::chrono_literals;
 const tick config::gameSpeed::fps = 33;
 tick config::gameSpeed::time_between_shrink = 45_s;
 
-unsigned config::chance::spawn_asteroide = 2_s;
-unsigned config::chance::spawn_powerup = 12_s;
+unsigned config::chance::spawn_asteroide = 3_s;
+unsigned config::chance::spawn_powerup = 5500_ms;
 
 std::chrono::milliseconds config::duration::time_between_firing = 500ms;
 std::chrono::milliseconds config::duration::time_between_firing_fast = 200ms;
@@ -26,4 +26,4 @@ tick config::operator"" _s(unsigned long long s) {
 }
 
 unsigned config::players::max_life = 1000;
-unsigned config::players::hurt_by_shot = 10;
+unsigned config::players::hurt_by_shot = 15;
